9c819a9083ef10ea298fa37183ac6b81d91655ea
lavi02 <18ds10535@dshs.kr>
2023-05-17 19:23:21 +0900

    「add: testUnits for Database」user-register system

--- test/roomTest.py
+++ test/roomTest.py
@@ -1,7 +1,16 @@
 from backend.test.__init__ import testInit
+from src.conn import *
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
 
 
 class roomTest(testInit):
     def generateRoomCode(self):
         roomNumLengh = len(self.roomNumber)
         self.assertEqual(roomNumLengh, 16)
+
+    def readRoomNum(self):
+        res = client.get("/api/v1/seed/generate")
+        self.assertEqual(res.status_code == 201)
+        self.assertEqual(res.json() == {"room_number": ""})
